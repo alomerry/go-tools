2bc0aa41019769f9a70cc8310cef5cc36bb2858d
alomerry.wu <alomerry.wu@gmail.com>
2026-03-14 01:25:36 +0800

    feat: refine apollo

--- components/apollo/apollo.go
+++ components/apollo/apollo.go
@@ -1,24 +1,24 @@
 package apollo
 
 import (
-	"encoding/json"
+  "encoding/json"
   "fmt"
   "log"
-	"reflect"
-	"strings"
-	"sync"
-
-	"github.com/alomerry/go-tools/static/cons"
-	"github.com/alomerry/go-tools/static/env"
-	"github.com/apolloconfig/agollo/v4"
-	"github.com/apolloconfig/agollo/v4/env/config"
+  "reflect"
+  "strings"
+  "sync"
+  
+  "github.com/alomerry/go-tools/static/cons"
+  "github.com/alomerry/go-tools/static/env"
+  "github.com/apolloconfig/agollo/v4"
+  "github.com/apolloconfig/agollo/v4/env/config"
 )
 
 var (
-	initOnce = sync.Once{}
+  initOnce = sync.Once{}
   
-  client apolloClient
-	listener *apolloListener
+  client   apolloClient
+  listener *apolloListener
 )
 
 type apolloClient struct {
@@ -27,40 +27,40 @@ type apolloClient struct {
 }
 
 func Init(appId, clientId string) {
-	initOnce.Do(func() {
-		c := &config.AppConfig{
-			AppID:          appId,
-			Cluster:        env.ApolloCluster(),
-			IP:             env.ApolloHost(),
-			IsBackupConfig: false,
-			MustStart:      true,
-			NamespaceName:  env.ApolloNamespace(),
-			Secret:         env.ApolloSK(),
-		}
+  initOnce.Do(func() {
+    c := &config.AppConfig{
+      AppID:          appId,
+      Cluster:        env.ApolloCluster(),
+      IP:             env.ApolloHost(),
+      IsBackupConfig: false,
+      MustStart:      true,
+      NamespaceName:  env.ApolloNamespace(),
+      Secret:         env.ApolloSK(),
+    }
     
     cli, err := agollo.StartWithConfig(func() (*config.AppConfig, error) {
-			return c, nil
-		})
-
-		if err != nil {
-			log.Panicf("init apollo failed, err: %v", err)
-		}
+      return c, nil
+    })
+    
+    if err != nil {
+      log.Panicf("init apollo failed, err: %v", err)
+    }
     client = apolloClient{cli, clientId}
-
-		listener = newApolloListener()
-		client.AddChangeListener(listener)
-	})
+    
+    listener = newApolloListener()
+    client.AddChangeListener(listener)
+  })
 }
 
 func Get(name string) (any, error) {
-	cache := client.GetConfigCache(env.ApolloNamespace())
-	value, err := cache.Get(name)
-	return value, err
+  cache := client.GetConfigCache(env.ApolloNamespace())
+  value, err := cache.Get(toKey(client.clientId, name))
+  return value, err
 }
 
 func GetJson[T any](name string, dist *T) error {
 	cache := client.GetConfigCache(env.ApolloNamespace())
-  value, err := cache.Get(strings.TrimSuffix(fmt.Sprintf("%s.%s", client.clientId, name), ",dynamic"))
+  value, err := cache.Get(strings.TrimSuffix(toKey(client.clientId, name), ",dynamic"))
 	if err != nil {
 		return err
 	}
@@ -70,13 +70,17 @@ func GetJson[T any](name string, dist *T) error {
 		err = json.Unmarshal([]byte(value.(string)), dist)
 	default:
     log.Panicf("unsupported type %v", reflect.TypeOf(value))
-	}
+  }
+  
+  if err != nil {
+    return err
+  }
+  
+  _ = listener.TryWatchKey(name, cons.ApolloValTypeJson, dist)
+  
+  return nil
+}
 
-	if err != nil {
-		return err
-	}
-
-	_ = listener.TryWatchKey(name, cons.ApolloValTypeJson, dist)
-
-	return nil
+func toKey(clientId string, key string) string {
+  return fmt.Sprintf("%s.%s", clientId, key)
 }
