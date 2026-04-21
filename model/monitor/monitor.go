36ce781a22df3b95e1808ad199b3cc9423d805ca
alomerry.wu <alomerry.wu@gmail.com>
2026-04-21 12:45:50 +0800

    feat: stats support docker

--- model/monitor/monitor.go
+++ model/monitor/monitor.go
@@ -5,13 +5,22 @@ import "time"
 type SystemStats struct {
 	Timestamp time.Time
 
+	Id          string
+	Name        string
 	PhysicalCPU int
 	LogicalCPU  int
-	CPUUsage    float64
+	CpuUsage    float64
+	UserUsage   float64
+	SystemUsage float64
 
-	TotalMemory uint64
-	UsedMemory  uint64
-	MemoryUsage float64
+	TotalMemory     uint64
+	CachedMemory    uint64
+	UsedMemory      uint64
+	MaxMemory       uint64
+	RssMemory       uint64
+	PageFaults      uint64
+	PageMajorFaults uint64
+	MemoryUsage     float64
 
 	DiskUsage map[string]float64
 	LoadAvg   [3]float64
